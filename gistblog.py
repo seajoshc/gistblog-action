1406902c8aac47ce8cf96915f0939967c5af2633
Josh Campbell <josh@p4a.net>
2021-12-04 14:37:45 -0800

    refactor to lookup gists using the api versus trying to parse a readme, pylint fixes

--- gistblog.py
+++ gistblog.py
@@ -2,9 +2,12 @@
 # Turn a blog into a bunch of gists
 
 import argparse
+import os
 from pathlib import Path
+
 from github import Github, InputFileContent
 
+token = os.environ.get('GISTS_TOKEN')
 
 parser = argparse.ArgumentParser()
 parser.add_argument("token", help="GitHub Personal Access Token with only 'Gists' scope. More info at https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/creating-a-personal-access-token.")
@@ -12,39 +15,57 @@ parser.add_argument("operation", help="create or update")
 parser.add_argument(
     "blog", help="Space delimited list of all files to upload to Gists.")
 args = parser.parse_args()
-
 print("Blog posts to process\n {}\n  {}".format(args.operation, args.blog))
 
 g = Github(args.token)
 github_user = g.get_user()
 
-for post in args.blog.split(","):
+for post in args.blog.split(" "):
     print("Processing {}".format(post))
+    post_title = ""
+    post_description = ""
+    post_file_name = post.split("/")[1]
+    # Parse Title and Description out of blog post
+    with open(post, encoding="utf-8") as file:
+        for line in file:
+            if "Title:" in line:
+                post_title = line.split("Title:")[1]
+            if "Description:" in line:
+                post_description = line.split("Description:")[1]
+            if post_title and post_description:
+                break
+
     if args.operation == "create":
         # Create a dict with file name and content
         d = {}
-        d[post.split("/")[1]] = InputFileContent(Path(post).read_text())
+        d[post_file_name] = InputFileContent(
+            Path(post).read_text(encoding="utf-8"))
 
         # Create a new gist
-        new_post = github_user.create_gist(True, d, post)
+        new_gist = github_user.create_gist(True, d, post_description)
 
-        # Parse Title out of blog post by searching for first "#"
-        new_post_title = "Blog Post"  # Default title
-        with open(post) as file:
-            for line in file:
-                if "#" in line:
-                    new_post_title = line.split("#")[1].strip()
-                    break
+        # Print out new row for blog table of contents
+        print("\n| Title | Published  | Id |")
+        print("\n| [{}]({}) | {} | {} |".format(
+            post_title.replace("\n", ""),
+            new_gist.html_url,
+            new_gist.created_at.strftime("%Y-%m-%d"),
+            post))
 
-        # Write the new post details to our blog table
-        with open("gists.md", "a") as gists:
-            gists.write("| [{}]({}) | {} | {} |".format(
-                new_post_title,
-                new_post.html_url,
-                new_post.created_at.strftime("%Y-%m-%d"),
-                new_post.id)
-            )
+    if args.operation == "update":
+        all_gists = github_user.get_gists()
+        gist_id = ""
 
-        print(" {}: {}".format(post, new_post.id))
-    elif args.operation == "update":
-        print("todo")
+        # Find the id of the gist we need to update by looking
+        # at all gists and matching on filename of the blog post
+        for gist in all_gists:
+            if post_file_name in gist.files:
+                gist_id = gist.id
+                break
+
+        # Update the gist with the new content
+        gist_to_update = g.get_gist(gist_id)
+        gist_to_update.edit(description=post_description, files={
+            post_file_name: InputFileContent(Path(post).read_text(encoding="utf-8"))})
+
+        print(" Updated Gist with ID {}".format(gist_id))

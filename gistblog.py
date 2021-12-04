885bf4eb48ae84af41c3e80b4884870b1edc5cea
Josh Campbell <josh@p4a.net>
2021-12-04 15:08:09 -0800

    adds better comments, removes looking for token in env vars

--- gistblog.py
+++ gistblog.py
@@ -4,13 +4,11 @@ Blog your little heart out using GitHub Gists.
 """
 
 import argparse
-import os
 from pathlib import Path
 
 from github import Github, InputFileContent
 
-token = os.environ.get('GISTS_TOKEN')
-
+# Argument parser
 parser = argparse.ArgumentParser()
 parser.add_argument("token", help="GitHub Personal Access Token with only 'Gists' scope. More info at https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/creating-a-personal-access-token.")
 parser.add_argument("operation", help="create or update")
@@ -19,14 +17,22 @@ parser.add_argument(
 args = parser.parse_args()
 print("Blog posts to process\n {}\n  {}".format(args.operation, args.blog))
 
+# Validate arguments
+if args.operation != "create" or "update":
+    print("Invalid Operation: only 'create' or 'update' are allowed.")
+    exit(1)
+
+# Setup PyGithub
 g = Github(args.token)
 github_user = g.get_user()
 
+# Start processing all the blog posts
 for post in args.blog.split(" "):
     print("Processing {}".format(post))
     post_title = ""
     post_description = ""
     post_file_name = post.split("/")[1]
+
     # Parse Title and Description out of blog post
     with open(post, encoding="utf-8") as file:
         for line in file:
@@ -37,8 +43,9 @@ for post in args.blog.split(" "):
             if post_title and post_description:
                 break
 
+    # Create new blog posts
     if args.operation == "create":
-        # Create a dict with file name and content
+        # Create a dict with the file name and content
         d = {}
         d[post_file_name] = InputFileContent(
             Path(post).read_text(encoding="utf-8"))
@@ -55,6 +62,7 @@ for post in args.blog.split(" "):
             new_gist.created_at.strftime("%Y-%m-%d"),
             post))
 
+    # Update a blog post
     if args.operation == "update":
         all_gists = github_user.get_gists()
         gist_id = ""
